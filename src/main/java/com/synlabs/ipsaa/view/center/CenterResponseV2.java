984f6cf51210c44a2cf91cdcbc5f977cfe5fd577
shubh_27 <shubhamgulati81@gmail.com>
2018-08-02 15:23:38 +0530

    center responce v2 updated

--- src/main/java/com/synlabs/ipsaa/view/center/CenterResponseV2.java
+++ src/main/java/com/synlabs/ipsaa/view/center/CenterResponseV2.java
@@ -4,12 +4,10 @@ import com.synlabs.ipsaa.entity.center.Center;
 import com.synlabs.ipsaa.enums.AddressType;
 import com.synlabs.ipsaa.view.common.AddressResponse;
 
-public class CenterResponse extends CenterSummaryResponse
+public class CenterResponseV2 extends CenterSummaryResponse
 {
 
-  private ZoneResponse    zone;
-  private AddressResponse address;
-
+  //private ZoneResponse    zone;
   // shubham
   private String address;
   private String city;
@@ -17,22 +15,73 @@ public class CenterResponse extends CenterSummaryResponse
   private String zipcode;
   private String phone;
   private AddressType addressType;
+  private String zone;
 
-
-  public CenterResponse(Center center)
+  public CenterResponseV2(Center center)
   {
     super(center);
-    this.address = new AddressResponse(center.getAddress());
-    this.zone = new ZoneResponse(center.getZone());
+    AddressResponse address = new AddressResponse(center.getAddress());
+    this.address=address.getAddress();
+    this.city=address.getCity();
+    this.addressType=address.getAddressType();
+    this.zipcode=address.getZipcode();
+    this.state=address.getState();
+    this.zone = center.getZone().getName();
   }
 
-  public AddressResponse getAddress()
-  {
+  public String getAddress() {
     return address;
   }
 
-  public ZoneResponse getZone()
-  {
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public String getZipcode() {
+    return zipcode;
+  }
+
+  public void setZipcode(String zipcode) {
+    this.zipcode = zipcode;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public AddressType getAddressType() {
+    return addressType;
+  }
+
+  public void setAddressType(AddressType addressType) {
+    this.addressType = addressType;
+  }
+
+  public String getZone() {
     return zone;
+  }
+
+  public void setZone(String zone) {
+    this.zone = zone;
   }
 }
