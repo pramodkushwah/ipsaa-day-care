281301141ebf936e878b2db7a520e3c1610a19c7
Ajay Garg <ajaykumargarg01@gmail.com>
2018-08-07 14:32:46 +0530

    student attendance and staff attendance bug fixing

--- src/main/java/com/synlabs/ipsaa/jpa/StudentAttendanceRepository.java
+++ src/main/java/com/synlabs/ipsaa/jpa/StudentAttendanceRepository.java
@@ -1,26 +1,31 @@
 package com.synlabs.ipsaa.jpa;
 
-import com.synlabs.ipsaa.entity.attendance.StudentAttendance;
-import com.synlabs.ipsaa.entity.center.Center;
-import com.synlabs.ipsaa.entity.student.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
-
 import java.util.Date;
 import java.util.List;
 
-public interface StudentAttendanceRepository extends JpaRepository<StudentAttendance, Long> , QueryDslPredicateExecutor<StudentAttendance>
-{
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
-  int countByStudentAndAttendanceDate(Student student, Date date);
+import com.synlabs.ipsaa.entity.attendance.StudentAttendance;
+import com.synlabs.ipsaa.entity.center.Center;
+import com.synlabs.ipsaa.entity.student.Student;
 
-  StudentAttendance findByStudentAndAttendanceDate(Student student, Date date);
-  List<StudentAttendance> findByStudentAndCreatedDateBetween(Student student,Date from, Date to);
-  List<StudentAttendance> findByCenterAndAttendanceDateBetweenOrderByStudentAdmissionNumberAsc(Center center, Date from, Date to);
+public interface StudentAttendanceRepository
+		extends JpaRepository<StudentAttendance, Long>, QueryDslPredicateExecutor<StudentAttendance> {
 
+	int countByStudentAndAttendanceDate(Student student, Date date);
 
-  //Avneet
-  List<StudentAttendance> findByStudentInAndAttendanceDateOrderByIdAsc(List<Student> students,Date date);
-  List<StudentAttendance> findByStudentCenterInAndStudentActiveTrueAndAttendanceDateAndCheckoutNotNull(List<Center> center,Date date);
+	StudentAttendance findByStudentAndAttendanceDate(Student student, Date date);
+
+	List<StudentAttendance> findByStudentAndCreatedDateBetween(Student student, Date from, Date to);
+
+	List<StudentAttendance> findByCenterAndAttendanceDateBetweenOrderByStudentAdmissionNumberAsc(Center center,
+			Date from, Date to);
+
+	// Avneet
+	List<StudentAttendance> findByStudentInAndAttendanceDateOrderByStudentIdAsc(List<Student> students, Date date);
+
+	List<StudentAttendance> findByStudentCenterInAndStudentActiveTrueAndAttendanceDateAndCheckoutNotNull(
+			List<Center> center, Date date);
 
 }
