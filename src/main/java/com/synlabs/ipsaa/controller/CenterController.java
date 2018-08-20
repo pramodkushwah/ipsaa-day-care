dbf886c664bba130285791d78614ec417457118c
Ajay Garg <ajaykumargarg01@gmail.com>
2018-08-20 10:53:05 +0530

    Center Controller

--- src/main/java/com/synlabs/ipsaa/controller/CenterController.java
+++ src/main/java/com/synlabs/ipsaa/controller/CenterController.java
@@ -1,78 +1,78 @@
 package com.synlabs.ipsaa.controller;
 
-import com.synlabs.ipsaa.entity.center.Center;
-import com.synlabs.ipsaa.entity.common.User;
-import com.synlabs.ipsaa.ex.ValidationException;
-import com.synlabs.ipsaa.service.CenterService;
-import com.synlabs.ipsaa.service.EmployeeService;
-import com.synlabs.ipsaa.service.StudentService;
-import com.synlabs.ipsaa.service.UserService;
-import com.synlabs.ipsaa.view.center.CenterListRequest;
-import com.synlabs.ipsaa.view.center.CenterRequest;
-import com.synlabs.ipsaa.view.center.CenterResponse;
-import com.synlabs.ipsaa.view.center.CenterResponseV2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import static com.synlabs.ipsaa.auth.IPSAAAuth.Privileges.ADMIN_CENTER_LIST_READ;
+import static com.synlabs.ipsaa.auth.IPSAAAuth.Privileges.CENTER_DELETE;
+import static com.synlabs.ipsaa.auth.IPSAAAuth.Privileges.CENTER_READ;
+import static com.synlabs.ipsaa.auth.IPSAAAuth.Privileges.CENTER_WRITE;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.synlabs.ipsaa.auth.IPSAAAuth.Privileges.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.synlabs.ipsaa.entity.center.Center;
+import com.synlabs.ipsaa.ex.ValidationException;
+import com.synlabs.ipsaa.service.CenterService;
+import com.synlabs.ipsaa.view.center.CenterListRequest;
+import com.synlabs.ipsaa.view.center.CenterRequest;
+import com.synlabs.ipsaa.view.center.CenterResponse;
+import com.synlabs.ipsaa.view.center.CenterResponseV2;
 
 @RestController
 @RequestMapping("/api/center/")
-public class CenterController
-{
-  @Autowired
-  private CenterService centerService;
+public class CenterController {
+	@Autowired
+	private CenterService centerService;
 
+	@GetMapping
+	@Secured(CENTER_READ)
+	public List<CenterResponseV2> list(@RequestParam(required = false, name = "zone") String zone,
+			@RequestParam(required = false, name = "city") String city) {
+		return centerService.list(new CenterListRequest(zone, city)).stream().map(CenterResponseV2::new)
+				.collect(Collectors.toList());
+	}
 
-  @GetMapping
-  @Secured(CENTER_READ)
-  public List<CenterResponse> list(@RequestParam(required = false, name = "zone") String zone,
-                                   @RequestParam(required = false, name = "city") String city)
-  {
-    return centerService.list(new CenterListRequest(zone, city)).stream().map(CenterResponseV2::new).collect(Collectors.toList());
-  }
+	@GetMapping("all")
+	@Secured(ADMIN_CENTER_LIST_READ)
+	public List<CenterResponse> allCenters() {
+		return centerService.listAll().stream().map(CenterResponse::new).collect(Collectors.toList());
+	}
 
-  @GetMapping("all")
-  @Secured(ADMIN_CENTER_LIST_READ)
-  public List<CenterResponse> allCenters()
-  {
-    return centerService.listAll().stream().map(CenterResponse::new).collect(Collectors.toList());
-  }
+	@Secured(CENTER_WRITE)
+	@PostMapping
+	public CenterResponse createCenter(@RequestBody @Validated CenterRequest request, BindingResult result) {
+		if (result.hasErrors()) {
+			throw new ValidationException(result.toString());
+		}
+		return new CenterResponse(centerService.createCenter(request));
+	}
 
-  @Secured(CENTER_WRITE)
-  @PostMapping
-  public CenterResponse createCenter(@RequestBody @Validated CenterRequest request, BindingResult result)
-  {
-    if (result.hasErrors())
-    {
-      throw new ValidationException(result.toString());
-    }
-    return new CenterResponse(centerService.createCenter(request));
-  }
+	@Secured(CENTER_WRITE)
+	@PutMapping
+	public CenterResponse updateCenter(@RequestBody @Validated CenterRequest request, BindingResult result) {
+		if (result.hasErrors()) {
+			throw new ValidationException(result.toString());
+		}
 
-  @Secured(CENTER_WRITE)
-  @PutMapping
-  public CenterResponse updateCenter(@RequestBody @Validated CenterRequest request, BindingResult result)
-  {
-    if (result.hasErrors())
-    {
-      throw new ValidationException(result.toString());
-    }
+		Center center = centerService.updateCenter(request);
+		return new CenterResponse(center);
+	}
 
-    Center center = centerService.updateCenter(request);
-    return new CenterResponse(center);
-  }
-
-  @Secured(CENTER_DELETE)
-  @DeleteMapping(path = "{centerId}")
-  public void deleteCenter(@PathVariable Long centerId)
-  {
-    centerService.deleteCenter(new CenterRequest(centerId));
-  }
+	@Secured(CENTER_DELETE)
+	@DeleteMapping(path = "{centerId}")
+	public void deleteCenter(@PathVariable Long centerId) {
+		centerService.deleteCenter(new CenterRequest(centerId));
+	}
 }
