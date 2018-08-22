f059569691ecdeca302da2d36f3be3a0033229a6
shubh_27 <shubhamgulati81@gmail.com>
2018-08-22 17:59:15 +0530

    fix discount by gopal

--- src/main/webapp/mis/app/services/studentfee.service.js
+++ src/main/webapp/mis/app/services/studentfee.service.js
@@ -3,35 +3,35 @@ app.service('StudentFeeService', function ($http) {
 
     // initailize fee, discounts and final fee 
     service.initializeFee = function(fee) {
-      fee.annualFee = fee.annualFee | 0;
-      fee.discountAnnualCharges = fee.discountAnnualCharges | 0;
+      fee.annualFee = fee.annualFee || 0;
+      fee.discountAnnualCharges = fee.discountAnnualCharges || 0;
       fee.finalAnnualFee = fee.discountAnnualCharges
         ? fee.finalAnnualFee
-        : fee.annualFee | 0;
+        : fee.annualFee || 0;
 
-      fee.admissionCharges = fee.admissionCharges | 0;
-      fee.discountAdmissionCharges = fee.discountAdmissionCharges | 0;
+      fee.admissionCharges = fee.admissionCharges || 0;
+      fee.discountAdmissionCharges = fee.discountAdmissionCharges || 0;
       fee.finalAdmissionCharges = fee.discountAdmissionCharges
         ? fee.finalAdmissionCharges
-        : fee.admissionCharges | 0;
+        : fee.admissionCharges || 0;
 
-      fee.baseFee = fee.baseFee | 0;
-      fee.discountBaseFee = fee.discountBaseFee | 0;
+      fee.baseFee = fee.baseFee || 0;
+      fee.discountBaseFee = fee.discountBaseFee || 0;
       fee.finalBaseFee = fee.discountBaseFee
         ? (fee.finalBaseFee / 3).toFixed(2)
         : fee.baseFee | 0;
 
-      fee.securityDeposit = fee.securityDeposit | 0;
-      fee.discountSecurityDeposit = fee.discountSecurityDeposit | 0;
+      fee.securityDeposit = fee.securityDeposit || 0;
+      fee.discountSecurityDeposit = fee.discountSecurityDeposit || 0;
       fee.finalSecurityDeposit = fee.discountSecurityDeposit
         ? fee.finalSecurityDeposit
-        : fee.securityDeposit | 0;
+        : fee.securityDeposit || 0;
 
-      fee.transportFee = fee.transportFee | 0;
+      fee.transportFee = fee.transportFee || 0;
       fee.finalTransportFees = fee.transportFee ? fee.transportFee * 3 : 0;
 
-      fee.uniformCharges = fee.uniformCharges | 0;
-      fee.stationary = fee.stationary | 0;
+      fee.uniformCharges = fee.uniformCharges || 0;
+      fee.stationary = fee.stationary || 0;
     }
 
     service.calculateFinalFee = function(fee) {
@@ -83,7 +83,7 @@ app.service('StudentFeeService', function ($http) {
     //needs finalFee, cgst and sgst
     service.calculateGstFee = function (fee) {
       if (fee.formalSchool) {
-        fee.gstFee = (fee.finalAnnualFee + (fee.finalBaseFee * 3)) * 0.18;
+        fee.gstFee = ((Number(fee.finalAnnualFee) + (Number(fee.finalBaseFee) * 3)) * 0.18).toFixed(2);
       // } else {
       //   if (fee.program.code === 'ICLUB' || fee.program.code === 'CLUBREG') {
       //     fee.gstFee = (fee.finalAnnualFee + fee.finalBaseFee * 3) * 0.18;
