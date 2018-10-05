7183530ff5f097affe5d3431b2e6ddb2d2f91696
avnib <avnibachu@gmail.com>
2018-10-05 18:05:11 +0530

    Staff Profile Request added

--- src/main/java/com/synlabs/ipsaa/view/staff/StaffProfileRequest.java
+++ src/main/java/com/synlabs/ipsaa/view/staff/StaffProfileRequest.java
@@ -54,14 +54,7 @@ public class StaffProfileRequest implements Request
     employeeProfile.setDoj(doj == null ? new Date() : doj);
     employeeProfile.setDol(dol);
     employeeProfile.setGender(gender);
-    if( employeeProfile.isNew()){
-      employeeProfile.setpState(pState);      ///set null on first time
-    }else{
-      /*do nothing for now
-      * remove if-else when pstate is added from frontend;*/
-    }
-
-
+    employeeProfile.setpState(pState);      ///set null on first time
     employeeProfile.setPan(pan);
     employeeProfile.setUan(uan);
     employeeProfile.setPfan(pfan);
@@ -267,10 +260,6 @@ public class StaffProfileRequest implements Request
   public String getspouseName() { return spouseName; }
 
   public void setspouseName(String spouseName) { this.spouseName = spouseName; }
-
-  public String getSpouseName() { return spouseName; }
-
-  public void setSpouseName(String spouseName) { this.spouseName = spouseName; }
 
   public String getpState() { return pState; }
 
