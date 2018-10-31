69cc2a28b3d394ad7511e3b2eb16869f3cdb32b2
ptech123 <kharetwal.pankaj111@gmail.com>
2018-10-31 12:16:44 +0530

    Ipsaa-club feepayment

--- src/main/webapp/mis/app/controllers/ipsaaclubslip.controller.js
+++ src/main/webapp/mis/app/controllers/ipsaaclubslip.controller.js
@@ -7,73 +7,118 @@ app.controller('IpsaaclubslipController', function ($scope, $http, Auth) {
     $scope.centers = response.data;
   });
 
-  $scope.getGeneratedFeeSlips = function(){    
-    if($scope.selectedCenter) {
-      $http.post('/api/student/ipsaaclub/feeslip/list', {'centerCode' : $scope.selectedCenter.code}).then(function(response){
+  $scope.getGeneratedFeeSlips = function () {
+    if ($scope.selectedCenter) {
+      $http.post('/api/student/ipsaaclub/feeslip/list', { 'centerCode': $scope.selectedCenter.code }).then(function (response) {
         $scope.generatedFeeSlips = response.data;
       })
     }
   }
 
+  $scope.selected = {};
 
- $scope.loadStudentFee = function(studentFee){
+  $scope.loadStudentFee = function (studentFee) {
     $scope.selectedStudentFee = studentFee;
   }
 
-  $scope.showPayNow = function(studentFee){
-    $scope.paynow = studentFee;
+  $scope.showPayNow = function (studentFee) {
+    $scope.selected = angular.copy(studentFee);
+    $scope.selected.paymentDate = moment(new Date()).format("YYYY-MM-DD");
   }
 
   $scope.downloadSlip = function (slip) {
     console.log(slip);
-        $scope.downloadPdfDisable = true;
-        $http.post('/api/student/ipsaaclub/feeslips/pdf', [slip.id], {responseType: 'arraybuffer'}).then(
-            function (response) {
-                $scope.downloadPdfDisable = false;
-                var blob = new Blob([response.data], {
-                    type: 'application/octet-stream'
-                });
-                saveAs(blob, response.headers("fileName"));
-            }, function (response) {
-                $scope.downloadPdfDisable = false;
-                error(response.data.error);
-            }
-        );
-};
- $scope.saveSlip = function(slip){
-    if($scope.selectedCenter) {
+    $scope.downloadPdfDisable = true;
+    $http.post('/api/student/ipsaaclub/feeslips/pdf', [slip.id], { responseType: 'arraybuffer' }).then(
+      function (response) {
+        $scope.downloadPdfDisable = false;
+        var blob = new Blob([response.data], {
+          type: 'application/octet-stream'
+        });
+        saveAs(blob, response.headers("fileName"));
+      }, function (response) {
+        $scope.downloadPdfDisable = false;
+        error(response.data.error);
+      }
+    );
+  };
+  $scope.saveSlip = function (slip) {
+    if ($scope.selectedCenter) {
       $http.post('/api/student/ipsaaclub/slip/update', {
-
-      extraCharge: $scope.selected.extraCharge,
-      id:slip.id
-      }).then(function(response){
-        $scope.generatedFeeSlips = response.data;
+        extraCharge: $scope.selectedStudentFee.extraCharge,
+        id: slip.id
+      }).then(function (response) {
+        ok('Extra Charges saved, Now you have to pay');
       })
     }
   }
   $scope.downloadRecipt = function (payment) {
-    console.log(payment);
-        $scope.downloadPdfDisable = true;
-        $http.get('/api/student/download/ipssaclub/receipt/' + payment.recordId, {}).then(
-            function (response) {
-                $scope.downloadPdfDisable = false;
-                var blob = new Blob([response.data], {
-                    type: 'application/octet-stream'
-                });
-                saveAs(blob, response.headers("fileName"));
-            }, function (response) {
-                $scope.downloadPdfDisable = false;
-                error(response.data.error);
-            }
-        );
-};
-    $scope.payFee = function(insertStudentFee) {
-        $http.post('/api/student/ipsaaclub/generate/' + insertStudentFee.studentId, {}).then(function (response){
-            ok('Student Fee generated');
-        },function(error){
-            error('Somthing went wrong');
-        })
+    $scope.downloadPdfDisable = true;
+    $http.get('/api/student/download/ipssaclub/receipt/' + payment.recordId, {}).then(
+      function (response) {
+        $scope.downloadPdfDisable = false;
+        var blob = new Blob([response.data], {
+          type: 'application/octet-stream'
+        });
+        saveAs(blob, response.headers("fileName"));
+      }, function (response) {
+        $scope.downloadPdfDisable = false;
+        error(response.data.error);
+      }
+    );
+  };
+  $scope.payFee = function (insertStudentFee) {
+    $http.post('/api/student/ipsaaclub/generate/' + insertStudentFee.studentId, {}).then(function (response) {
+      ok('Student Fee generated');
+    }, function (error) {
+      error('Somthing went wrong');
+    })
+  }
+
+  $scope.cancelStudentSlip = function () {
+    $scope.showPanel = false;
+    $scope.selected = {};
+  };
+
+  $scope.payStudentFee = function () {
+    var fee_diff = $scope.selected.paidAmount - $scope.selected.payableAmount;
+    if(!($scope.selected && $scope.selected.paidAmount)){
+      error("Please enter paid amount.");
+      return ;
     }
+    if(fee_diff > 0){
+      swal({
+        type: 'warning',
+        title: 'Confirmation',
+        text: 'Your are about to pay extra amount of Ruppes '+ fee_diff + '.',
+        confirmButtonText: 'Proceed',
+        showCancelButton: 'true'
+      }).then( function() {
+          //proceed callback
+          $scope.feePaymentRequest();
+        }, function (cancel) {}
+      );
+    } else {
+      $scope.feePaymentRequest();
+    }
+  };
+
+  $scope.feePaymentRequest = function() {
+    $scope.disabledRecordPayment = true;
+    $http.post('/api/student/ipsaaclub/payfee', $scope.selected).then(function (response) {
+      $scope.disabledRecordPayment = false;
+      ok("Successfully applied payment")
+    }, function (response) {
+      $scope.disabledRecordPayment = false;
+      error(response.data.error);
+    });
+  }
+
+  $scope.addExtraCharges = function (){
+    $scope.selectedStudentFee.totalFee = $scope.selectedStudentFee.finalFee;
+    $scope.selectedStudentFee.totalFee += $scope.selectedStudentFee.extraCharge;
+    
+  }
 
   function error(message) {
     swal({
