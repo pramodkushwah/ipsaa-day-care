ce434f830a27d1d5abefd021f1c3843958acf78c
ptech123 <kharetwal.pankaj111@gmail.com>
2018-11-21 12:10:00 +0530

    Added ipsaaclub component

---
+++ src/main/angular/src/pages/mis/fee/ipsaaclub/ipsaaclub.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ipsaaclub',
+  templateUrl: './ipsaaclub.component.html',
+  styleUrls: ['./ipsaaclub.component.css']
+})
+export class IpsaaclubComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
