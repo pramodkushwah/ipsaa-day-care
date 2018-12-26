d6b9c92385ebfd2be3aa8acf7352d10d3f7e9b5c
pramodkushwah <pkushwah758@gmail.com>
2018-12-26 11:58:00 +0530

    dashboard icon changes

--- src/main/angular/src/pages/mis/dashboard/dashboard.component.ts
+++ src/main/angular/src/pages/mis/dashboard/dashboard.component.ts
@@ -78,27 +78,7 @@ export class DashboardComponent implements OnInit{
   }
 
   // ngAfterViewInit(){
-  //   if ($('#back-to-top').length) {
-  //     var scrollTrigger = 100, // px
-  //         backToTop = function () {
-  //             var scrollTop = $(window).scrollTop();
-  //             if (scrollTop > scrollTrigger) {
-  //                 $('#back-to-top').addClass('show');
-  //             } else {
-  //                 $('#back-to-top').removeClass('show');
-  //             }
-  //         };
-  //     backToTop();
-  //     $(window).on('scroll', function () {
-  //         backToTop();
-  //     });
-  //     $('#back-to-top').on('click', function (e) {
-  //         e.preventDefault();
-  //         $('html,body').animate({
-  //             scrollTop: 0
-  //         }, 700);
-  //     });
-  // }
+    
   // }
 
   loadDashboard() {
